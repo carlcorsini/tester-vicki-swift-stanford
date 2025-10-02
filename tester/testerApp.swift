10927a260ad3f1071e7b512a34aa852ae8b7ede1
Carl Corsini <carl.m.corsini@gmail.com>
2025-10-02 10:49:27 -0700

    Initial Commit

---
+++ tester/testerApp.swift
@@ -0,0 +1,17 @@
+//
+//  testerApp.swift
+//  tester
+//
+//  Created by Carl Corsini on 10/2/25.
+//
+
+import SwiftUI
+
+@main
+struct testerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ testerTests/testerTests.swift
@@ -0,0 +1,17 @@
+//
+//  testerTests.swift
+//  testerTests
+//
+//  Created by Carl Corsini on 10/2/25.
+//
+
+import Testing
+@testable import tester
+
+struct testerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
